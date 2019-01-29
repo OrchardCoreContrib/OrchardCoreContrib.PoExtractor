542133459e116a8f6cda4e38037c1ade8291be58
Lukas Kabrt <lukas@kabrt.cz>
2019-01-29 23:18:45 +0100

    Release version 0.1.0 (#14)
    * Create NuGet.config
    
    * Correct NuGet feed for the OrchardCore preview
    
    * Set up CI with Azure Pipelines
    
    * Fixed test task in Azure Pipelines (#13)
    
    * Pack application as dotnet global tool (#12)
    
    * Update azure-pipelines.yml

---
+++ PoExtractor/Program.cs
@@ -0,0 +1,60 @@
+﻿using PoExtractor.Core;
+using PoExtractor.Core.Contracts;
+using PoExtractor.CS;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PoExtractor {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 2) {
+                WriteHelp();
+                return;
+            }
+
+            var basePath = args[0];
+            var outputBasePath = args[1];
+
+            string[] projectFiles;
+            if (Directory.Exists(basePath)) {
+                projectFiles = Directory.EnumerateFiles(basePath, "*.csproj", SearchOption.AllDirectories).ToArray();
+            } else {
+                WriteHelp();
+                return;
+            }
+
+            var processors = new IProjectProcessor[] {
+                new CSharpProjectProcessor()
+            };
+
+            foreach (var projectFilePath in projectFiles) {
+                var projectPath = Path.GetDirectoryName(projectFilePath);
+                var projectBasePath = Path.GetDirectoryName(projectPath) + Path.DirectorySeparatorChar;
+                var projectRelativePath = projectPath.TrimStart(basePath + Path.DirectorySeparatorChar);
+                var outputPath = Path.Combine(outputBasePath, Path.GetFileNameWithoutExtension(projectFilePath) + ".pot");
+
+                var strings = new LocalizableStringCollection();
+                foreach (var processor in processors) {
+                    processor.Process(projectPath, projectBasePath, strings);
+                }
+
+                if (strings.Values.Any()) {
+                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+
+                    using (var potFile = new PoWriter(outputPath)) {
+                        potFile.WriteRecord(strings.Values);
+                    }
+                }
+
+                Console.WriteLine($"{Path.GetFileName(projectPath)}: Found {strings.Values.Count()} strings.");
+            }
+        }
+
+        private static void WriteHelp() {
+            Console.WriteLine("Usage: extractpo input output");
+            Console.WriteLine("    input: path to the input directory, all projects at the the path will be processed");
+            Console.WriteLine("    output: path to a directory where POT files will be generated");
+        }
+    }
+}
