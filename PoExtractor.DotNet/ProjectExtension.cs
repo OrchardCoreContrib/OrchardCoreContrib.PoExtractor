1ad4d1f119ae53484efcd445120307ac5c83e545
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-12-19 00:44:18 +0300

    Introduce ProjectExtension

---
+++ PoExtractor.DotNet/ProjectExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace PoExtractor.DotNet
+{
+    public class ProjectExtension
+    {
+        public static readonly string CS = ".csproj";
+
+        public static readonly string VB = ".vbproj";
+    }
+}
