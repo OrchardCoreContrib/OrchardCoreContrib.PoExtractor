1665b652d55bdc08e09cc40e18e561103d8750e1
Lukas Kabrt <lukas@kabrt.cz>
2020-05-10 21:45:25 +0200

    Added T localizer accessor

--- PoExtractor.DotNet/LocalizerAccessors.cs
+++ PoExtractor.DotNet/LocalizerAccessors.cs
@@ -2,10 +2,12 @@
 {
     public static class LocalizerAccessors
     {
+        public static readonly string DefaultLocalizerIdentifier = "T";
+
         public static readonly string StringLocalizerIdentifier = "S";
 
         public static readonly string HtmlLocalizerIdentifier = "H";
 
-        public static string[] LocalizerIdentifiers = new string[] { StringLocalizerIdentifier, HtmlLocalizerIdentifier };
+        public static string[] LocalizerIdentifiers = new string[] { DefaultLocalizerIdentifier, StringLocalizerIdentifier, HtmlLocalizerIdentifier };
     }
 }
