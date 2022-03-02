b8868674fb75631fea63b03a53bfae40ba5eeafa
Michael Lüpkes <mluepkes@users.noreply.github.com>
2022-03-02 11:57:03 +0100

    Update LocalizerAccessors.cs
    Added accessors with proper private field names.

--- PoExtractor.DotNet/LocalizerAccessors.cs
+++ PoExtractor.DotNet/LocalizerAccessors.cs
@@ -4,10 +4,22 @@
     {
         public static readonly string DefaultLocalizerIdentifier = "T";
 
+        public static readonly string DefaultLocalizerPrivateIdentifier = "_t";
+
         public static readonly string StringLocalizerIdentifier = "S";
+
+        public static readonly string StringLocalizerPrivateIdentifier = "_s";
 
         public static readonly string HtmlLocalizerIdentifier = "H";
 
-        public static string[] LocalizerIdentifiers = new string[] { DefaultLocalizerIdentifier, StringLocalizerIdentifier, HtmlLocalizerIdentifier };
+        public static readonly string HtmlLocalizerPrivateIdentifier = "_h";
+
+        public static string[] LocalizerIdentifiers = new string[] {    DefaultLocalizerIdentifier, 
+                                                                        DefaultLocalizerPrivateIdentifier, 
+                                                                        StringLocalizerIdentifier, 
+                                                                        StringLocalizerPrivateIdentifier, 
+                                                                        HtmlLocalizerIdentifier, 
+                                                                        HtmlLocalizerPrivateIdentifier 
+                                                                    };
     }
 }
