1a69efe6d1509d2307b057a8f96cc84fa74727b9
Hisham Bin Ateya <hishamco_2007@hotmail.com>
2022-12-04 14:51:25 +0300

    Add CLI options for language and template engine

---
+++ src/OrchardCoreContrib.PoExtractor/TemplateEngine.cs
@@ -0,0 +1,11 @@
+﻿namespace OrchardCoreContrib.PoExtractor
+{
+    public class TemplateEngine
+    {
+        public static readonly string Liquid = "Liquid";
+
+        public static readonly string Razor = "Razor";
+
+        public static readonly string Both = string.Empty;
+    }
+}
