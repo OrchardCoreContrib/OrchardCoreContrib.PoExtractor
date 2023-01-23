392fa0f042567af8876780d3686443935248c71f
Ryan Powers <Ryan.Powers@advantagecs.com>
2023-01-23 14:42:26 -0500

    Use IStringLocalizer instead of FakeStringLocalizer

--- test/OrchardCoreContrib.PoExtractor.Tests/ProjectFiles/StringLocalizerExample.cs
+++ test/OrchardCoreContrib.PoExtractor.Tests/ProjectFiles/StringLocalizerExample.cs
@@ -1,10 +1,11 @@
-﻿using OrchardCoreContrib.PoExtractor.Tests.Fakes;
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
 
 namespace OrchardCoreContrib.PoExtractor.Tests.ProjectFiles
 {
     public class StringLocalizerExample
     {
-        private readonly FakeStringLocalizer S = new();
+        private readonly IStringLocalizer S;
 
         public void Singular()
         {
@@ -13,7 +14,7 @@ namespace OrchardCoreContrib.PoExtractor.Tests.ProjectFiles
 
         public void Plural()
         {
-            S.Plural(1, "{0} thing", "{0} things");
+            _ = S.Plural(1, "{0} thing", "{0} things");
         }
     }
 }
