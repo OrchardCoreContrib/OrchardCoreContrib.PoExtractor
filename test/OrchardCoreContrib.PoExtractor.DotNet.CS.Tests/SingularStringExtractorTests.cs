a7d0814ee7c0e88a8028eeded30e8e60f6e77765
Bruno Juchli <bruno.juchli@smart-me.com>
2024-02-26 15:24:44 +0100

    add test for extracting verbatim multiline string, deduplicate test code

--- test/OrchardCoreContrib.PoExtractor.DotNet.CS.Tests/SingularStringExtractorTests.cs
+++ test/OrchardCoreContrib.PoExtractor.DotNet.CS.Tests/SingularStringExtractorTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using OrchardCoreContrib.PoExtractor.DotNet.CS.MetadataProviders;
 
@@ -5,15 +6,20 @@ namespace OrchardCoreContrib.PoExtractor.DotNet.CS.Tests;
 
 public class SingularStringExtractorTests
 {
+    private readonly SingularStringExtractor extractor;
+    
+    public SingularStringExtractorTests()
+    {
+        var metadataProvider = new CSharpMetadataProvider("DummyBasePath");
+        extractor = new SingularStringExtractor(metadataProvider);
+    }
+    
     [Fact]
     public void ExtractString()
     {
         // Arrange
         var text = "Thing";
-        var metadataProvider = new CSharpMetadataProvider("DummyBasePath");
-        var extractor = new SingularStringExtractor(metadataProvider);
-
-        var syntaxTree = CSharpSyntaxTree.ParseText($"S[\"{text}\"];", path: "DummyPath");
+        var syntaxTree = ParseSyntaxTree($"S[\"{text}\"];");
         
         var node = syntaxTree
             .GetRoot()
@@ -29,26 +35,14 @@ public class SingularStringExtractorTests
     }
 
     [Fact]
-    public void ExtractRawLiteralString()
+    public void ExtractVerbatimMultilineString()
     {
         // Arrange
-        var text = """
-                   Thing
-                   in a literal string
-                   """;
-        var metadataProvider = new CSharpMetadataProvider("DummyBasePath");
-        var extractor = new SingularStringExtractor(metadataProvider);
-
-        var syntaxTree = CSharpSyntaxTree.ParseText(
+        var syntaxTree = ParseSyntaxTree(
             """
-            S[
-                \"\"\"
-                Thing
-                in a literal string
-                \"\"\"
-            ];
-            """,
-            path: "DummyPath");
+            S[@"Thing
+                in a literal string"];
+            """);
         
         var node = syntaxTree
             .GetRoot()
@@ -60,6 +54,47 @@ public class SingularStringExtractorTests
 
         // Assert
         Assert.True(extracted);
-        Assert.Equal(text, result.Text);
+        Assert.Equal(
+            $"Thing{Environment.NewLine}    in a literal string",
+            result.Text);
+    }
+
+    [Fact]
+    public void ExtractRawLiteralString()
+    {
+        // Arrange
+        var syntaxTree = ParseSyntaxTree(
+            """
+            S[
+                \"\"\"
+                Thing
+                in a literal string
+                \"\"\"
+            ];
+            """);
+        
+        var node = syntaxTree
+            .GetRoot()
+            .DescendantNodes()
+            .ElementAt(2);
+
+        // Act
+        var extracted = extractor.TryExtract(node, out var result);
+
+        // Assert
+        Assert.True(extracted);
+        Assert.Equal(
+            """
+            Thing
+            in a literal string
+            """,
+            result.Text);
+    }
+
+    private static SyntaxTree ParseSyntaxTree(string sourceCode)
+    {
+        return CSharpSyntaxTree.ParseText(
+            sourceCode,
+            path: "DummyPath");
     }
 }
