284e8b35b165887a72fb7568462f6791ae139bfa
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-16 16:01:37 +0100

    bug fix

--- src/OrchardCoreContrib.PoExtractor/Program.cs
+++ src/OrchardCoreContrib.PoExtractor/Program.cs
@@ -121,7 +121,7 @@ public class Program
                     projectProcessor.Process(projectPath, projectBasePath, localizableStrings);
                 }
 
-                if (isSingleFileOutput)
+                if (!isSingleFileOutput)
                 {
                     if (localizableStrings.Values.Any())
                     {
