ffac438cf2ebf8e05077fb4a7e8571c4cad479ca
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-01-22 02:14:23 +0300

    Delete src/OrchardCoreContrib.PoExtractor/GetCliOptionsResult.cs

--- src/OrchardCoreContrib.PoExtractor/GetCliOptionsResult.cs
+++
@@ -1,9 +0,0 @@
-namespace OrchardCoreContrib.PoExtractor;
-
-public class GetCliOptionsResult
-{
-    public string Language { get; set; }
-    public string TemplateEngine { get; set; }
-    public string SingleOutputFile { get; set; }
-    public IList<string> Plugins { get; set; } = new List<string>();
-}
